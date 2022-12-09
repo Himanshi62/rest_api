3bae28d1bf02e660cecdce2e93df30bb2050438d
Himanshi Yadav <himanshi.iitb62@gmail.com>
2022-12-10 00:25:17 +0530

    function based api view

--- tmdb_app/api/serializers.py
+++ tmdb_app/api/serializers.py
@@ -7,3 +7,14 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField()
     release_date = serializers.CharField()
     isHit = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.isHit = validated_data.get('isHit', instance.isHit)
+        instance.save()
+        return instance
--- tmdb_app/api/views.py
+++ tmdb_app/api/views.py
@@ -7,15 +7,38 @@ from rest_framework.decorators import api_view
 from tmdb_app.api.serializers import MovieSerializer
 
 # Create your views here.
-@api_view()
+@api_view(["GET","POST"])
 def list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many = True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many = True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
-@api_view()
+@api_view(["GET","PUT","DELETE"])
 def movie_detail(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    if request.method == "PUT":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+        
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
